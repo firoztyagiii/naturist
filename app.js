30dbb9d964a063cae927d2d2cf7058263590ccc9
Firoz Tyagi <firoztyagi1783@gmail.com>
2021-10-16 21:35:51 +0530

    CORS headers added

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", domain);
   }
   res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
 
