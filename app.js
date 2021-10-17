7507fc311d5e6e187a5a5c54046a8d4449333f5f
Firoz Tyagi <firoztyagi1783@gmail.com>
2021-10-17 13:39:10 +0530

    Fixed CORS headers

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", domain);
   }
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
   next();
 });
