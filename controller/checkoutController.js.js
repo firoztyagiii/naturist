658ac8074202c4f04fc4c1a62e71600c27311107
Firoz Tyagi <firoztyagi1783@gmail.com>
2021-10-26 20:25:08 +0530

    razor added

--- controller/checkoutController.js.js
+++ controller/checkoutController.js.js
@@ -1,11 +1,9 @@
 const Model = require("../model/allModels");
 const Razorpay = require("razorpay");
 
-const secret_key = "aEoBGobKOlheWd6RJeVzoyPW";
-
 const rzrpy = new Razorpay({
   key_id: "rzp_test_iQXxC9LBZRPfH9",
-  key_secret: secret_key,
+  key_secret: "aEoBGobKOlheWd6RJeVzoyPW",
 });
 
 exports.getCheckoutSession = async (req, res, next) => {
@@ -19,7 +17,7 @@ exports.getCheckoutSession = async (req, res, next) => {
         reference_id: tour._id,
         username: req.user.name,
         email: req.user.email,
-        userId: user.req._id,
+        user: req.user._id,
       },
     });
     res.status(200).json({ order: order });
@@ -28,12 +26,4 @@ exports.getCheckoutSession = async (req, res, next) => {
   }
 };
 
-exports.confirmCheckout = async (req, res, next) => {
-  console.log("BODY ===>", req.body);
-  const signature = req.body.response.razorpay_order_id + "|" + req.body.response.razorpay_payment_id;
-  console.log(signature);
-  var expectedSignature = crypto.createHmac("sha256", secret_key).update(signature.toString()).digest("hex");
-  if (expectedSignature === req.body.response.razorpay_signature) {
-    console.log(true);
-  }
-};
+exports.confirmCheckout = async (req, res, next) => {};
