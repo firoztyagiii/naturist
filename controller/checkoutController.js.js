e9f38cc20a546b7a0b26987665950e3e4e73df69
Firoz Tyagi <firoztyagi1783@gmail.com>
2021-11-01 17:41:02 +0530

    Adding Webhookd

--- controller/checkoutController.js.js
+++ controller/checkoutController.js.js
@@ -27,13 +27,5 @@ exports.getCheckoutSession = async (req, res, next) => {
 };
 
 exports.confirmCheckout = async (req, res, next) => {
-  let body = req.body.response.razorpay_order_id + "|" + req.body.response.razorpay_payment_id;
-
-  var crypto = require("crypto");
-  var expectedSignature = crypto.createHmac("sha256", "aEoBGobKOlheWd6RJeVzoyPW").update(body.toString()).digest("hex");
-  console.log("sig received ", req.body.response.razorpay_signature);
-  console.log("sig generated ", expectedSignature);
-  var response = { signatureIsValid: "false" };
-  if (expectedSignature === req.body.response.razorpay_signature) response = { signatureIsValid: "true" };
-  res.send(response);
+  console.log(req.body);
 };
