f50d104ae512e765feb833777151b864d1bd061d
Firoz Tyagi <firoztyagi1783@gmail.com>
2021-11-02 12:12:04 +0530

    Bookings Finished

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const authController = require("../controller/authController");
 const bookingController = require("../controller/bookingController");
 
-router.route("/my-bookings").get(authController.isLoggedIn, bookingController.getBookings);
+router.route("/").get(authController.isLoggedIn, bookingController.getBookings);
 
 module.exports = router;
